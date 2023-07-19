0a5282e25d9d9f420d98715109a7be413a0a9072
nastiasalionova <asalionova@yandez.ru>
2023-07-19 17:58:16 +0500

    first

--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, top_sellers
 
 urlpatterns = [
-    path ('', index)
+    path('', index, name='main-page'),
+    path('top-sellers', top_sellers, name='top-sellers'),
+    path('index', index, name='index')
 ]
 
 
