fff88b1003ecfbe5afe7a8543cec583a39cb3467
nastiasalionova <asalionova@yandez.ru>
2023-07-26 23:03:53 +0500

    second

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -7,7 +7,17 @@ def index(request):
 def top_sellers(request):
     return render(request, 'top-sellers.html')
 
+def advertisement_post(request):
+    return render(request, 'advertisement-post.html')
 
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def profile(request):
+    return render(request, 'profile.html')
 
 
 # Create your views here.
